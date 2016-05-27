d72549a63badc29bb3016206f3ace84af55d5449
Duncan Hunter <duncan@duncanhunter.com.au>
2016-05-28 05:52:05 +0700

    fixing styles on home page

--- MusicStore/src/app/album/album-summary.component.ts
+++ MusicStore/src/app/album/album-summary.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input} from '@angular/core';
+import * as md from '../angular-material/index'
 import {Album} from '../models';
 
 @Component({
@@ -6,7 +7,12 @@ import {Album} from '../models';
     selector: 'ms-album-summary',
     templateUrl: 'album-summary.component.html',
     styleUrls: ['album-summary.component.css'],
-    directives: []
+    directives: [
+        md.MD_CARD_DIRECTIVES,
+        md.MdCard,
+        md.MdCardHeader,
+        md.MdCardTitleGroup
+    ]
 })
 
 export class AlbumSummaryComponent  {
