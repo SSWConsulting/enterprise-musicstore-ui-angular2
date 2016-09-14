3e01f34386d9bdb4c8dbd3e233abf87d7e51ef9f
Adam Stephensen <mail@adamstephensen.com>
2016-09-14 19:57:07 +1000

    Update the Auth0 credentials

--- MusicStore/src/app/config.ts
+++ MusicStore/src/app/config.ts
@@ -1,6 +1,6 @@
 export const API_BASE = 'https://musicstore-api-aspnet.azurewebsites.net/api';
 //export const API_BASE = 'https://localhost:44343/api';
 export const APP_BASE = 'http://localhost:5000';
-export const AUTH0_CLIENT_ID = 'ikMjOLanjOske624faAjYe0Ve7cAOg33';
-export const AUTH0_DOMAIN = 'ssw.au.auth0.com';
+export const AUTH0_CLIENT_ID = 'OMadkLUeTx821YM1awtrNF1wDlEk69XT';
+export const AUTH0_DOMAIN = 'ssw.auth0.com';
 export const STRIPE_PUBLISHABLE_KEY = 'pk_test_k1DBtWFqUyKqAXqyR0n7AIRr';
