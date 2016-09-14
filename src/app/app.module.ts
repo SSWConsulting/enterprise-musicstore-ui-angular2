6aeb61055b8af4db46a2f95764db4343ce2eb3b2
Adam Stephensen <mail@adamstephensen.com>
2016-09-14 21:02:15 +1000

    Import and use the components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,26 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdButtonModule } from '@angular2-material/button';
+import { MdButtonToggleModule } from '@angular2-material/button-toggle';
+import { MdCardModule } from '@angular2-material/card';
+import { MdCheckboxModule } from '@angular2-material/checkbox';
+import { MdCoreModule } from '@angular2-material/core';
+import { MdGridListModule } from '@angular2-material/grid-list';
+import { MdIconModule } from '@angular2-material/icon';
+import { MdInputModule } from '@angular2-material/input';
+import { MdListModule } from '@angular2-material/list';
+import { MdMenuModule } from '@angular2-material/menu';
+import { MdProgressBarModule } from '@angular2-material/progress-bar';
+import { MdProgressCircleModule } from '@angular2-material/progress-circle';
+import { MdRadioModule } from '@angular2-material/radio';
+import { MdSidenavModule } from '@angular2-material/sidenav';
+import { MdSliderModule } from '@angular2-material/slider';
+import { MdSlideToggleModule } from '@angular2-material/slide-toggle';
+import { MdTabsModule } from '@angular2-material/tabs';
+import { MdToolbarModule } from '@angular2-material/toolbar';
+import { MdTooltipModule } from '@angular2-material/tooltip';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +32,27 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdCoreModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdProgressBarModule,
+    MdProgressCircleModule,
+    MdRadioModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
