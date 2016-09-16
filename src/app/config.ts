4b95a2d7e28761b568ea12067bde028f35e50b6b
Adam Stephensen <mail@adamstephensen.com>
2016-09-16 23:09:58 +1000

    Add Auth0

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,1 +1,4 @@
 export const API_BASE = 'http://musicstore-api-aspnet.azurewebsites.net/api';
+
+export const AUTH0_CLIENT_ID = 'OMadkLUeTx821YM1awtrNF1wDlEk69XT';
+export const AUTH0_DOMAIN = 'ssw.auth0.com';
